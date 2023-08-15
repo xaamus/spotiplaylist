675b98971a9f09d259e7b62cd2a4ba74bbdf5ba4
xaamus <kacper.samus@gmail.com>
2023-08-15 16:52:43 +0200

    css

--- scripts.js
+++ scripts.js
@@ -2,17 +2,13 @@ let container = document.getElementById('container');
 function button(){
     text = '';
     k=0;
-    o=0
     for (j=0;j<Song.length;j++){
-        if (Added[k].toLowerCase().includes('')){
-            o +=1 ;
-            text += '<div class="row_ele">';
-            text += '<img class="imags" src="'+Image[k]+'">';
-            text += '<p class="par_song">'+Song[k]+'</p>';
-            text += '<p class="par_arts">'+Artist[k]+'</p>';
-            text += '<p class="par_rlsd">'+Release[k].slice(0,4)+'</p>';
-            text += '</div>';
-        }
+        text += '<div class="row_ele">';
+        text += '<img class="imags" src="'+Image[k]+'">';
+        text += '<p class="par_song">'+Song[k]+'</p>';
+        text += '<p class="par_arts">'+Artist[k]+'</p>';
+        text += '<p class="par_rlsd">'+Release[k].slice(0,4)+'</p>';
+        text += '</div>';
         k += 1;
     }
     container.innerHTML = text;
